c2ee589319b573e408589e0a76cf721066794521
Andrei <139020977+AxinitM@users.noreply.github.com>
2024-09-15 18:11:26 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+# Random quotes generator
+
+The page was created as part of the [Front End Developer Course by Orange DC](https://digitalcenter.orange.md/).
+
+## Table of contents
+
+
+- [Random quotes generator](#random-quotes-generator)
+  - [Table of contents](#table-of-contents)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Tasks](#tasks)
+  - [Random Quote Generator Description](#random-quote-generator-description)
+  - [Author](#author)
+
+## Screenshot
+
+![](./image/screenshot.png)
+
+## Links
+
+[Random quotes generator Page]()
+
+## Tasks
+Create a page with a quote generator. 
+
+## Random Quote Generator Description
+This page displays random quotes and their authors, retrieving this information via the API from https://zenquotes.io. 
+
+The page uses two fonts: mainFontFamily (for the welcome message, error message, and request limit exceeded message) and QuoteHandWritingFont (for the quote). 
+
+If the quote is successfully fetched, it is displayed in the font (var(--QuoteHandWritingFont)). The author’s name is shown in the #author element. 
+
+If an error occurs while fetching the quote, an error message is displayed. The font for the error message is the main font (var(--mainFontFamily)). 
+
+If the API returns a request limit exceeded message, the quote text changes to inform about the need to purchase a key or wait 30 seconds. For better readability, this message is displayed using the main font (var(--mainFontFamily)).
+
+The page is adapted for screens 470 pixels and smaller.
+## Author
+
+[Andrei Martinenko](https://github.com/AxinitM)
---
+++ script.js
@@ -0,0 +1,50 @@
+// Receiving a random quote using the ZenQuotes API via CORS Proxy
+async function fetchQuote() {
+  const apiURL = `https://api.allorigins.win/get?url=https://zenquotes.io/api/random&timestamp=${new Date().getTime()}`;
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    const quoteData = JSON.parse(data.contents)[0];
+
+    // Checking API's "Too many requests" message
+    if (
+      quoteData.q ===
+      "Too many requests. Obtain an auth key for unlimited access."
+    ) {
+      document.getElementById(
+        "quote"
+      ).textContent = `Too many requests. Buy an auth key for unlimited access, or wait 30 seconds and press the button again!`;
+
+      // Setting font and font-size for "Too many requests" message
+      document.getElementById("quote").style.fontFamily =
+        "var(--mainFontFamily)";
+      document.getElementById("quote").style.fontSize = "1rem";
+      document.getElementById("author").style.fontStyle = "normal";
+      // document.getElementById("author").style.fontSize = "1rem";
+    } else {
+      // Updating the quote and author text on the page
+      document.getElementById("quote").textContent = `"${quoteData.q}"`;
+      document.getElementById("quote").style.fontSize = "1.8rem";
+      document.getElementById("author").style.fontStyle = "italic";
+      // document.getElementById("author").style.fontSize = "1.2rem";
+      
+
+      // Setting font to handwriting font for normal quotes
+      document.getElementById("quote").style.fontFamily =
+        "var(--QuoteHandWritingFont)";
+    }
+
+    document.getElementById("author").textContent = `- ${quoteData.a}`;
+  } catch (error) {
+    console.error("Error fetching the quote:", error);
+    document.getElementById("quote").textContent =
+      "An error occurred, please try again!";
+
+    // Main Font for Error message
+    document.getElementById("quote").style.fontFamily = "var(--mainFontFamily)";
+    document.getElementById("author").textContent = "";
+  }
+}
+
+// Event listener for button to generate a new quote
+document.getElementById("new-quote").addEventListener("click", fetchQuote);
